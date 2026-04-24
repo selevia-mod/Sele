89158bbcfff05f20dd6dfc273a3da45dea2aa2c9
selevia-mod <seleviacruz@gmail.com>
2026-04-25 00:25:09 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -276,7 +276,6 @@ function renderPost(post) {
           `).join('')}
         </div>
       </div>
-      <div class="reaction-summary" id="rsummary-${post.id}"></div>
       <button class="comment-toggle" data-postid="${post.id}">
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/></svg>
         <span id="ccount-${post.id}">Comment</span>
